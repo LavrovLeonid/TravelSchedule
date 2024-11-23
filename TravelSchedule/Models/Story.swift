1d510449cfd1006dcc6c392a1e8ff40744433be7
Lavrov Leonid <l.lavrov@kari.com>
2024-11-23 10:44:28 +0500

    feat: Добавлена структура истории

---
+++ TravelSchedule/Models/Story.swift
@@ -0,0 +1,44 @@
+//
+//  Story.swift
+//  TravelSchedule
+//
+//  Created by Леонид Лавров on 11/22/24.
+//
+
+import Foundation
+
+struct Story: Identifiable {
+    let id = UUID()
+    let image: ImageResource
+    let previewImage: ImageResource
+    let title: String
+    let description: String
+    
+    static let story1: Story = .init(
+        image: .story1,
+        previewImage: .story1Preview,
+        title: "Text Text Text Text Text Text Text Text Text Text",
+        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+    )
+    
+    static let story2: Story = .init(
+        image: .story2,
+        previewImage: .story2Preview,
+        title: "Text Text Text Text Text Text Text Text Text Text",
+        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+    )
+    
+    static let story3: Story = .init(
+        image: .story3,
+        previewImage: .story3Preview,
+        title: "Text Text Text Text Text Text Text Text Text Text",
+        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+    )
+    
+    static let story4: Story = .init(
+        image: .story4,
+        previewImage: .story4Preview,
+        title: "Text Text Text Text Text Text Text Text Text Text",
+        description: "Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text"
+    )
+}
