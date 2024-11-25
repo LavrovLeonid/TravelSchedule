afb77a7eb81dbf9adface69eb1068c58540423b6
Lavrov Leonid <l.lavrov@kari.com>
2024-11-25 09:26:36 +0500

    chore: Доработана структура Story

--- TravelSchedule/Models/Story.swift
+++ TravelSchedule/Models/Story.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Story: Identifiable {
+struct Story: Identifiable, Hashable, Equatable {
     let id = UUID()
     let image: ImageResource
     let previewImage: ImageResource
