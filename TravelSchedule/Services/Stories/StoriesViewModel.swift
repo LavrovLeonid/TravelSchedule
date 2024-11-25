956fc4dbc909b0a23089b2dd5ffb831333f03a2a
Lavrov Leonid <l.lavrov@kari.com>
2024-11-25 09:28:35 +0500

    feat: Добавлен environmentObject для StoriesViewModel

--- TravelSchedule/Services/Stories/StoriesViewModel.swift
+++ TravelSchedule/Services/Stories/StoriesViewModel.swift
@@ -11,8 +11,23 @@ import Foundation
 final class StoriesViewModel: ObservableObject {
     @Published var stories: [Story] = [.story1, .story2, .story3, .story4]
     @Published var viewedStories = Set<UUID>()
+    @Published var currentStoryIndex: Int = 0 {
+        didSet {
+            guard let story = stories[safe: currentStoryIndex] else { return }
+            
+            viewedStories.insert(story.id)
+        }
+    }
+    @Published var currentProgress: CGFloat = 0
     
-    func viewStory(_ story: Story) {
-        viewedStories.insert(story.id)
+    var timerConfiguration: TimerConfiguration {
+        .init(storiesCount: stories.count)
+    }
+    
+    func storyPreviewTapped(_ story: Story) {
+        guard let index = stories.firstIndex(of: story) else { return }
+        
+        currentStoryIndex = index
+        currentProgress = timerConfiguration.progress(for: index)
     }
 }
